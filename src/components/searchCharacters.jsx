63d0ec07bb05a274917b443e298e6a1a067769b5
Bogdan Barjaktarevic <barjaktarevicbogdan@gmail.com>
2022-10-09 15:59:44 +0200

    add searching and first search logic

--- src/components/searchCharacters.jsx
+++ src/components/searchCharacters.jsx
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
-import { Form, useLoaderData, useSubmit } from "react-router-dom";
+import {
+  Form,
+  useLoaderData,
+  useSubmit,
+  useNavigation,
+} from "react-router-dom";
 
 const SearchCharacters = () => {
   const { q } = useLoaderData();
   const submit = useSubmit();
+  const navigation = useNavigation();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
 
   useEffect(() => {
     document.getElementById("q").value = q;
   }, [q]);
 
   const handleOnChange = (event) => {
-    submit(event.currentTarget.form);
+    const isFirstSearch = q == null;
+    submit(event.currentTarget.form, {
+      replace: !isFirstSearch,
+    });
   };
 
   return (
@@ -18,6 +31,7 @@ const SearchCharacters = () => {
       <Form id="search-form" role="search">
         <input
           id="q"
+          className={searching ? "loading" : ""}
           aria-label="Search contacts"
           placeholder="Search"
           type="search"
@@ -25,6 +39,8 @@ const SearchCharacters = () => {
           defaultValue={q}
           onChange={handleOnChange}
         />
+        <div id="search-spinner" aria-hidden hidden={!searching} />
+        <div className="sr-only" aria-live="polite"></div>
       </Form>
     </div>
   );
