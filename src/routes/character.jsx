ec870300f6761400f6a7ebdf09270dede162a435
Bogdan Barjaktarevic <barjaktarevicbogdan@gmail.com>
2022-10-20 15:01:18 +0200

    style single character page

--- src/routes/character.jsx
+++ src/routes/character.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getCharacter } from "../service/api/marvelApi";
 
 export async function loader({ params }) {
@@ -10,22 +10,75 @@ const Character = () => {
   const character = useLoaderData();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <h1>{character.name}</h1>
-      <h5>Appeared in: {character.comics.available} comics</h5>
-      <p>{character.description}</p>
-      <img
-        width="300"
-        height="300"
-        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-        alt={character.name}
-      />
+    <div className="bg-slate-200 h-screen flex md:justify-center md:items-center mx-auto flex-col">
+      <div className="p-4 bg-red-500 rounded-full	mb-4 hover:bg-red-600 self-center mt-2">
+        <Link to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 fill-white stroke-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </Link>
+      </div>
+      <div className="flex flex-col md:flex-row md:container shadow-md rounded-md bg-slate-100 p-6">
+        <div className="max-w-sm">
+          <img
+            className="rounded-md"
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt={character.name}
+          />
+        </div>
+        <div className="grow mt-4 md:mt-0 md:ml-6 flex flex-col justify-between">
+          <div>
+            <h5 className="font-bold text-slate-700">{character.name}</h5>
+            <h5 className="mt-2">Description</h5>
+            <p className="text-xs">{character.description}</p>
+          </div>
+          <div className="flex justify-between mt-8 md:mt-0">
+            <div className="flex flex-col items-center">
+              <span className="mb-2 text-3xl font-extrabold">
+                {character.comics.available}
+              </span>
+              <p className="font-light text-gray-500 dark:text-gray-400">
+                Comics
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="mb-2 text-3xl font-extrabold">
+                {character.events.available}
+              </span>
+              <p className="font-light text-gray-500 dark:text-gray-400">
+                Events
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="mb-2 text-3xl font-extrabold">
+                {character.series.available}
+              </span>
+              <p className="font-light text-gray-500 dark:text-gray-400">
+                Series
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="mb-2 text-3xl font-extrabold">
+                {character.stories.available}
+              </span>
+              <p className="font-light text-gray-500 dark:text-gray-400">
+                Stories
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
